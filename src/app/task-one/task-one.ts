7ac44a2eeb196afd085d94e91d27a113b29963f0
tamilmanimurugan <tamilmani.murugan@infinire.com>
2026-04-10 12:10:16 +0530

    dev

--- src/app/task-one/task-one.ts
+++ src/app/task-one/task-one.ts
@@ -8,24 +8,18 @@ import { Component } from '@angular/core';
 
 export class TaskOne {
   isDisabled: boolean = true;
+  message: string = "";
+  name: string = "Tamil";     // Interpolation data
+  imageUrl: string = "images/Layer1.png";   // Property binding 
+  count: number = 0;       //  Count variable
   showMessage() {
     this.message = "Hello " + this.name + "!";
   }
-  message: string = "";
-
-  // Interpolation data
-  name: string = "Tamil";
-
-  // Property binding 
-  imageUrl: string = "images/Layer 1.png";
 
   // Event binding
   showMessage1() {
     alert("Hello, " + this.name + "!");
   }
-
-  //  Count variable
-  count: number = 0;
 
   //  Increase count
   increase() {
